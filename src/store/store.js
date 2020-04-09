b43d641a9f442aa79052a407e3bd1732b6f0b790
alexzhaohong <alexhong96@gmail.com>
2020-04-09 10:49:08 -0700

    add redux dev tools:

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
-const store = createStore(rootReducer, applyMiddleware(thunk));
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+//const store = createStore(reducer, /* preloadedState, */ composeEnhancers(
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
